73ec5b9819e617b2eb2adf6ebbcbb6ad994df604
deividhere <play3rman@gmail.com>
2024-04-29 23:15:39 +0300

    added semi-blank webpage and favicon

---
+++ www/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Clinică medicală</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="icon" href="./assets/favicon.ico" type="image/x-icon">
+  </head>
+  <body>
+    <main>
+        <h1>MASE</h1>
+
+        <?php
+        echo "Today is " . date("Y/m/d") . "<br>";
+        echo "Today is " . date("Y.m.d") . "<br>";
+        echo "Today is " . date("Y-m-d") . "<br>";
+        echo "Today is " . date("l");
+        ?>
+        
+    </main>
+	<script src="index.js"></script>
+  </body>
+</html>
