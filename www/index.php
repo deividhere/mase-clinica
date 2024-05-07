a5b49b15b644d0b986ac612d45edab7f9e4e41ab
deividhere <play3rman@gmail.com>
2024-05-07 15:46:11 +0300

    installed bootstrap

--- www/index.php
+++ www/index.php
@@ -7,55 +7,19 @@
     <title>Clinică medicală</title>
     <link rel="stylesheet" type="text/css" href="./style/style.css">
     <link rel="icon" href="./assets/favicon/favicon.ico" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </head>
   <body>
-    <main>
-        <h1>MASE</h1>
+    
+    <div class="container">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sagittis vulputate urna non placerat. Mauris mollis egestas risus id iaculis. Vestibulum id tellus mollis, scelerisque lectus a, consequat augue. Vestibulum eleifend diam faucibus nisi consectetur vehicula. Donec vitae neque sed nibh blandit ornare. Sed a imperdiet quam, ut posuere ex. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus laoreet diam a tincidunt malesuada. Vestibulum at dignissim purus, quis ultricies urna.
 
-        <br>
+    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam nisl libero, sodales eget erat vel, molestie aliquam metus. Donec cursus cursus eros vel suscipit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus porta placerat enim, quis tincidunt metus sollicitudin sed. Donec ultricies arcu ut diam porttitor tempor. Sed a eros ac ante cursus tincidunt. Quisque pretium quis neque quis venenatis. Etiam efficitur mauris leo, sit amet tincidunt urna ullamcorper in. Phasellus at sem et purus pharetra fermentum sit amet at purus. Fusce vulputate lacus nec quam commodo congue.
 
-        <!-- Button to open the modal login form -->
-        <button onclick="document.getElementById('id01').style.display='block'" class="genericbtn">Login</button>
-
-        <!-- The Modal -->
-        <div id="id01" class="modal">
-          <span onclick="document.getElementById('id01').style.display='none'"
-        class="close" title="Close Modal">&times;</span>
-
-          <!-- Modal Content -->
-          <form class="modal-content animate" action="/action_page.php">
-            <div class="imgcontainer">
-              <img src="./assets/avatar.png" alt="Avatar" class="avatar">
-            </div>
-
-            <div class="container">
-              <label for="uname"><b>Username</b></label>
-              <input type="text" placeholder="Enter Username" name="uname" required>
-
-              <label for="psw"><b>Password</b></label>
-              <input type="password" placeholder="Enter Password" name="psw" required>
-
-              <button type="submit">Login</button>
-              <label>
-                <input type="checkbox" checked="checked" name="remember"> Remember me
-              </label>
-            </div>
-
-            <div class="container">
-              <div style="text-align: center;">
-                <span style="font-weight: bold;">Not registered?</span>
-                <span style=""><a href="#">Create an account!</a></span>
-              </div>
-            </div>
-
-            <div class="container" style="background-color:#f1f1f1">
-              <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-              <span class="psw">Forgot <a href="#">password?</a></span>
-            </div>
-          </form>
-        </div>
-
-    </main>
+    Pellentesque in tellus nunc. Praesent nec erat sit amet nulla hendrerit pharetra. Quisque nisl nibh, semper eu libero sit amet, placerat porttitor nisl. Sed mattis id velit at volutpat. Etiam a tortor viverra, mattis dui in, pulvinar dolor. Donec et est non sapien pellentesque pellentesque. Sed id tristique velit, non porttitor risus. Vivamus at sapien et dui dapibus finibus. Proin nec dui neque. Vestibulum vitae eros sit amet nisi varius malesuada et nec nunc. Aliquam quis nunc nec nisi tincidunt scelerisque. Vivamus nec purus in felis congue volutpat. Nullam ultricies elementum accumsan. Suspendisse dolor arcu, ornare nec laoreet at, vehicula nec nibh.
+    </div>
+    
 	<script src="script.js"></script>
   </body>
 </html>
